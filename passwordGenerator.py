6fbc4ef89b1149d34a4bdb3e0e5f30143a2abeed
Matheus Freitas <teusanjos0@gmail.com>
2021-01-24 21:58:49 -0300

    Update 1

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,36 +1,54 @@
 import random
 import string
 
-letraMaiuscula = string.ascii_uppercase
-letraMinuscula = string.ascii_lowercase
+def gerador(tamanho, maiusculo, minusculo, numero):
+
+    letraMaiuscula = string.ascii_uppercase
+    letraMinuscula = string.ascii_lowercase
+
+    senha = []
+
+    for _ in range(tamanho):
+
+        escolha = random.randint(1,3)
+
+        if escolha == 1:
+            if maiusculo == 1:
+                caracterMa = ''.join(random.choice(letraMaiuscula))
+                senha.append(caracterMa)
+            else:
+                caracterMa = ""
+                while caracterMa == "":
+                    caracterMa = gerador(1, maiusculo, minusculo, numero)
+                senha.append(caracterMa)
+
+        elif escolha == 2:
+            if minusculo == 1:
+                caracterMi = ''.join(random.choice(letraMinuscula))
+                senha.append(caracterMi)
+            else:
+                caracterMi = ""
+                while caracterMi == "":
+                    caracterMi = gerador(1, maiusculo, minusculo, numero)
+                senha.append(caracterMi)
+
+        elif escolha == 3:
+            if numero == 1:
+                numeral = str(random.randint(0,9))
+                senha.append(numeral)
+            else:
+                numeral = ""
+                while numeral == "":
+                    numeral = gerador(1, maiusculo, minusculo, numero)
+                senha.append(numeral)
+        
+    senhaFinal = ''.join(senha)
+
+    return senhaFinal
 
 tamanho = int(input("Digite o tamanho da sua senha: "))
-
 escolhaMai = int(input("\nDeseja letra maiuscula? \nSim - 1 \nNão - 0\n   Resposta: "))
 escolhaMin = int(input("\n\nDeseja letra minuscula? \nSim - 1 \nNão - 0\n   Resposta: "))
 escolhaNum = int(input("\n\nDeseja numero? \nSim - 1 \nNão - 0\n   Resposta: "))
 
-senha = []
-
-for _ in range(tamanho):
-
-    escolha = random.randint(1,3)
-
-    if escolha == 1:
-        if escolhaMai == 1:
-            caracter = ''.join(random.choice(letraMaiuscula))
-            senha.append(caracter)
-
-    if escolha == 2:
-        if escolhaMin == 1:
-            caracter = ''.join(random.choice(letraMinuscula))
-            senha.append(caracter)
-
-    if escolha == 3:
-        if escolhaNum == 1:
-            numeral = str(random.randint(0,9))
-            senha.append(numeral)
-    
-senhaFinal = ''.join(senha)
-
-print("A senha final é: " + senhaFinal)
+print("A senha final é: " + gerador(tamanho, escolhaMai, escolhaMin, escolhaNum))
